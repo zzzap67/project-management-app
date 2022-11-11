6f1c1c834fe93bd21f57579d9d3a7597593d2357
polina <polina@mail.ru>
2022-11-11 16:02:26 +0300

    feat: add developer's links in footer

--- project-management-app/src/components/Footer/Footer.tsx
+++ project-management-app/src/components/Footer/Footer.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './Footer.css';
+import data from '../ui/developer-card/developers.json';
 
 const Footer: React.FunctionComponent = () => {
   return (
     <footer className="footer">
+      <div className="developer_links">
+        {data.map((item, index: number) => (
+          <a href={item.href} key={index} className="github_link">
+            {item.github}
+          </a>
+        ))}
+      </div>
       <a className="rss_link" href="https://rs.school/react/">
         © 2022 The Rolling Scopes
       </a>
