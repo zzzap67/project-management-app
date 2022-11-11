d4ca4e09c04bcd6b1335cb60d828f0287b5baa94
polina <polina@mail.ru>
2022-11-11 14:56:31 +0300

    add rss link in footer

--- project-management-app/src/components/Footer/Footer.tsx
+++ project-management-app/src/components/Footer/Footer.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './Footer.css';
 
 const Footer: React.FunctionComponent = () => {
-  return <footer className="footer"> © 2022 The Rolling Scopes</footer>;
+  return (
+    <footer className="footer">
+      <a className="rss_link" href="https://rs.school/react/">
+        © 2022 The Rolling Scopes
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
