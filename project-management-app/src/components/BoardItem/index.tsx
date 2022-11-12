f60a385a29613b5950964fc837f53c93fceb67f5
polina <polina@mail.ru>
2022-11-12 17:50:29 +0300

    fix: fix BoardItem component

--- project-management-app/src/components/BoardItem/index.tsx
+++ project-management-app/src/components/BoardItem/index.tsx
@@ -1,15 +1,31 @@
 import { IBoard } from 'types';
+import { ReactComponent as Delete } from '../../assets/icons/delete.svg';
+import { ReactComponent as Edit } from '../../assets/icons/edit.svg';
+import { ReactComponent as TaskBoard } from '../../assets/icons/task-board.svg';
 import './styles.css';
 
 const BoardItem = (props: IBoard) => {
   const { id, title, description } = props;
+  const deleteBoard = () => {
+    console.log('delete board');
+  };
+  const editBoard = () => {
+    console.log('edit board');
+  };
   return (
-    <>
-      <div>
-        <div>{title}</div>
-        <div>{description}</div>
+    <div className="board_item">
+      <div className="change_board">
+        <Edit className="edit_board" onClick={deleteBoard} />
+        <Delete className="delete_board" onClick={editBoard} />
       </div>
-    </>
+      <div className="board_info">
+        <TaskBoard className="task-board" />
+        <div className="info">
+          <p className="info_title">{title}</p>
+          <p className="info_description">{description}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- project-management-app/src/pages/main/index.tsx
+++ project-management-app/src/pages/main/index.tsx
@@ -1,7 +1,10 @@
+import BoardItem from 'components/BoardItem';
+
 const Main = () => {
   return (
     <>
       <div> WELCOME TO PROJECT MANAGEMENT APP </div>
+      <BoardItem id={'dfb'} title={'dfb'} description={'dfb'} />
     </>
   );
 };
