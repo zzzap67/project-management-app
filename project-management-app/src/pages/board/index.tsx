e385993512c54f0aa2933e90724208cbf875d547
polina <polina@mail.ru>
2022-11-14 18:57:21 +0300

    feat: add create new column button

---
+++ project-management-app/src/pages/board/index.tsx
@@ -0,0 +1,19 @@
+import Button from 'components/ui/button';
+import { t } from 'i18next';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from 'store/hooks';
+
+const Board = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  return (
+    <div className="board_page">
+      <Button
+        className="create_column__button"
+        buttonName={t('description.forms.createColumn')}
+        eventHandler={() => navigate('/board/:id/column')}
+      />
+    </div>
+  );
+};
+export default Board;
