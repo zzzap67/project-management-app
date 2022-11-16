6a8d09bb2907183543306a8806d3c476789ec021
zzzap67 <zzzap67@gmail.com>
2022-11-16 23:38:22 +0300

    feat: add columns to board

--- project-management-app/src/pages/board/index.tsx
+++ project-management-app/src/pages/board/index.tsx
@@ -1,12 +1,20 @@
 import ColumnList from 'components/ColumnList';
 import Button from 'components/ui/button';
 import { t } from 'i18next';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useAppDispatch } from 'store/hooks';
+import { getAllColumnsThunk } from 'store/thunks';
 
 const Board = () => {
+  const { id } = useParams();
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(getAllColumnsThunk(id as string));
+  }, [dispatch]);
+
   return (
     <div className="board_page">
       <div className="column_list">
