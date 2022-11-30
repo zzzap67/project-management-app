44a084dfc75a84773b2b8beccc15e1f6142ef3ce
polina <polina@mail.ru>
2022-11-30 14:33:14 +0300

    fix:fix  welcome page routing

--- project-management-app/src/App.tsx
+++ project-management-app/src/App.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 
 import './App.css';
-
 import Main from 'pages/main';
 import Error404 from 'pages/error404';
 import SignIn from 'pages/signIn';
